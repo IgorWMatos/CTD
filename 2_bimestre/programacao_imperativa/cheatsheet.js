0e1f41388df030ff195c3e151c347607888a7b76
Igor <iw-matos@hotmail.com>
2022-08-11 08:42:38 -0300

    cheatsheet upload

---
+++ 2_bimestre/programacao_imperativa/cheatsheet.js
@@ -0,0 +1,113 @@
+//
+//    ,-.       _,---._ __  / \
+//   /  )    .-'       `./ /   \
+//  (  (   ,'            `/    /|
+//   \  `-"             \'\   / |
+//    `.              ,  \ \ /  |
+//     /`.          ,'-`----Y   |
+//    (            ;        |   '
+//    |  ,-.    ,-'         |  /
+//    |  | (   |        hjw | /
+//    )  |  \  `.___________|/
+//    `--'   `--'
+//  
+
+/* ==========================
+ARITMÉTICOS
+========================== */
+
+// Nos permitem fazer operações matemáticas
+
+
+
+// 10 + 15 //---> Soma
+
+//exemplo:
+let somaDeDoisNumeros = 20 + 40;
+console.log("Utilizando o sinal de (+) podemos fazer operações de adição, por exemplo: " + somaDeDoisNumeros + ' ou ' + (2 + 2));
+
+/* As mulheres piscam mais do que os homens. Um estudo descobriu que elas piscam espontaneamente 19 vezes por minuto, enquanto eles piscam apenas 11 vezes */
+
+
+// 10 - 15 //---> Subtração
+
+//exemplo:
+let subtracaoDeDoisNumeros = 60 - 40;
+console.log("Utilizando o sinal de (-) podemos fazer operações de subtração, por exemplo: " + subtracaoDeDoisNumeros + ' ou ' + (6 - 4));
+
+/* Os fundadores da Hewlett-Packard fizeram cara ou coroa para decidir qual nome apareceria primeiro na empresa */
+
+// 10 * 15 //---> Multiplicação
+
+//exemplo:
+let multiplicacaoDeDoisNumeros = 2 * 2;
+console.log("Utilizando o sinal de (*) podemos fazer operações de multiplicação, por exemplo: " + multiplicacaoDeDoisNumeros + ' ou ' + (6 * 4));
+
+/* Sigmund Freud chegou a acreditar que a cocaína poderia ser usada para tratar o vício em morfina */
+
+
+// 10 / 15 //---> Divisão
+
+//exemplo
+let divisaoDeDoisNumeros = 4 / 2;
+console.log("Utilizando o sinal de (/) podemos fazer operações de divisão, por exemplo: " + divisaoDeDoisNumeros + ' ou ' + (6 / 4));
+
+/* A exuberante coloração dos flamingos vem dos carotenoides, que são pigmentos que eles conseguem através de alguns alimentos, como algas e camarões */
+
+
+// 15++    //---> Incremento de um em um: 15 + 1
+
+//exemplo
+let x = 3;
+const y = x++;
+
+console.log(`Utilizando o sinal de (++) podemos fazer operações de incremento, por exemplo: x:${x}, y:${y} (valor original de x era 3)`);
+
+/* É possível estimar a temperatura (em Fahrenheit) contando o número de vezes que um grilo estrila em 15 segundos e adicionar o número 37 */
+
+
+// 15--    //---> Decremento de um em um: 15 - 1
+
+//exemplo
+
+let c = 3;
+const z = c--;
+console.log(`Utilizando o sinal de (--) podemos fazer operações de incremento, por exemplo: x:${c}, y:${z} (valor original de c era 3)`);
+
+/* Nos Estados Unidos, a indústria de pizzas serve cerca de 100 acres de pizza todos os dias */
+
+
+// 15 % 5  //---> Módulo. O resto obtido da divisão de 15 por 5: 0
+// 15 % 2  //---> Módulo. O resto obtido da divisão de 15 por 2: 1
+
+//exemplo
+
+let moduloResto = 26 % 3;
+console.log("Utilizando o sinal de (%) podemos fazer operações de módulo, obtendo o resto de uma divisão, por exemplo: " + moduloResto + " ou " + (20 % 5));
+
+/* Os camelos têm três pálpebras para poder proteger seus olhos da areia */
+
+
+/* ==========================
+COMPARAÇÃO SIMPLES
+========================== */
+
+
+
+/* ==========================
+COMPARAÇÃO ESTRITA
+========================== */
+
+
+
+/* ==========================
+OPERADORES RELACIONAIS
+========================== */
+
+
+
+/* ==========================
+LÓGICOS
+========================== */
+
+// Permitem combinar valores booleanos e seu resultado ao mesmo tempo, também é um booleano
