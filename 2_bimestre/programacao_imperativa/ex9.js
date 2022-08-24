ead291ab8082ad2f0e3fa516dec78848a5893b48
Igor <iw-matos@hotmail.com>
2022-08-24 08:09:18 -0300

    alterações

---
+++ 2_bimestre/programacao_imperativa/ex9.js
@@ -0,0 +1,29 @@
+let filmes = ['star wars', 'totoro', 'rocky', 'pulp fiction', 'a vida é bela'];
+let filme = ["Toy Story", "Procurando Nemo", "Kung-fu Panda", "Wally", "Fortnite"];
+
+/* Exercício 1 */
+
+// for(let auxiliar = 0; auxiliar<filmes.length; auxiliar++){
+//     filmes[auxiliar] = " filme "+filmes[auxiliar].toUpperCase();
+// }
+// console.log(filmes);
+
+/* Exercício 2 */
+
+
+
+/* Exercício 3 */
+
+/* Exercício 4 */
+
+// const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+// const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+
+
+// for (let i = 0; i < asiaScores.length, i++;) {
+//     for (let j = 0; j < euroScores.length; i++) {
+//         if (asiaScores[i] == euroScores[j]) {
+//             console.log('mesma nota');
+//         };
+//     };
+// };
