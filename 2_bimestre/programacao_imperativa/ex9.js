419940ef533a942d606216a36da01446b0fd2f7c
Igor <iw-matos@hotmail.com>
2022-08-24 10:46:38 -0300

    alterações

--- 2_bimestre/programacao_imperativa/ex9.js
+++ 2_bimestre/programacao_imperativa/ex9.js
@@ -1,29 +1,35 @@
-let filmes = ['star wars', 'totoro', 'rocky', 'pulp fiction', 'a vida é bela'];
-let filme = ["Toy Story", "Procurando Nemo", "Kung-fu Panda", "Wally", "Fortnite"];
-
 /* Exercício 1 */
 
+// let filmes = ['star wars', 'totoro', 'rocky', 'pulp fiction', 'a vida é bela'];
+// let filme = ["Toy Story", "Procurando Nemo", "Kung-fu Panda", "Wally", "Fortnite"];
+
 // for(let auxiliar = 0; auxiliar<filmes.length; auxiliar++){
-//     filmes[auxiliar] = " filme "+filmes[auxiliar].toUpperCase();
+//     filmes[auxiliar] = filmes[auxiliar].toUpperCase();
 // }
 // console.log(filmes);
 
-/* Exercício 2 */
+/* Exercício 2 & 3 */
 
-
-
-/* Exercício 3 */
+// function passagemDeElementos(a, b) {
+//     let popElement = b.indexOf("Fortnite");
+//     b.splice(popElement,1);
+//     while (b.length > 0) {
+//         a.push(b.pop().toUpperCase());
+//     }
+// }
+// passagemDeElementos(filmes,filme);
+// console.log(filmes);
 
 /* Exercício 4 */
 
-// const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
-// const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
 
+function compare(asiaScores, euroScores) {
+    let compareAsiaEuro = [];
+    for (index = 0; index < asiaScores.length; index++) {
+        console.log(compareAsiaEuro[index] = asiaScores[index] === euroScores[index]);
+    };
+}
 
-// for (let i = 0; i < asiaScores.length, i++;) {
-//     for (let j = 0; j < euroScores.length; i++) {
-//         if (asiaScores[i] == euroScores[j]) {
-//             console.log('mesma nota');
-//         };
-//     };
-// };
+compare(asiaScores, euroScores)
