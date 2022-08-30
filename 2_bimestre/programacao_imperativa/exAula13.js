2d24bfc59f4a398c1b85ff1cfc5703427d8aa583
Igor Matos <105871904+IgorWMatos@users.noreply.github.com>
2022-08-29 22:20:50 -0300

    alteracoes

--- 2_bimestre/programacao_imperativa/exAula13.js
+++ 2_bimestre/programacao_imperativa/exAula13.js
@@ -5,30 +5,32 @@ function Banco(titular, nConta, tipoConta, saldo) {
     this.saldo = saldo;
 }
 
-let clienteUm = new Banco("Abigael Natte", 5486273622, "Conta Corrente", 27771);
+let client1 = ['Abigael Natte', 5486273622, 'Conta Corrente', 27771]
+let client2 = ['Ramon Connell', 1183971869, 'Conta Poupança', 8675]
+let client3 = ['Jarret Lafuente', 9582019689, 'Conta Poupança', 27363]
+let client4 = ['Ansel Ardley', 1761924656, 'Conta Poupança', 32415]
+let client5 = ['Jacki Shurmer', 7401971607, 'Conta Poupança', 18789]
+let client6 = ['Jobi Mawtus', 630841470, 'Conta Corrente', 28776]
+let client7 = ['Thomasin Latour', 4223508636, 'Conta Corrente', 2177]
+let client8 = ['Lonnie Verheijden', 185979521, 'Conta Poupança', 25994]
+let client9 = ['Alonso Wannan', 3151956165, 'Conta Corrente', 7601]
+let client10 = ['Bendite Huggett', 2138105881, 'Conta Poupança', 33196]
 
-let clienteDois = new Banco("Ramon Connell", 1183971869, "Poupança", 8675);
+// let clienteUm = new Banco("Abigael Natte", 5486273622, "Conta Corrente", 27771);
+// let clienteDois = new Banco("Ramon Connell", 1183971869, "Poupança", 8675);
+// let clienteTres = new Banco("Jarret Lafuente", 9582019689, "Conta Poupança", 27363);
+// let clienteQuatro = new Banco("Ansel Ardley", 1761924656, "Conta Poupança", 32415);
+// let clienteCinco = new Banco("Jacki Shurmer", 7401971607, "Conta Poupança", 18789);
+// let clienteSeis = new Banco("Jobi Mawtus", 630841470, "Conta Corrente", 28776);
+// let clienteSete = new Banco("Thomasin Latour", 4223508636, "Conta Corrente", 2177);
+// let clienteOito = new Banco("Lonnie Verheijden", 185979521, "Conta Poupança", 25994);
+// let clienteNove = new Banco("Alonso Wannan", 3151956165, "Conta Corrente", 7601);
+// let clienteDez = new Banco("Bendite Huggett", 2138105881, "Conta Poupança", 33196);
 
-let clienteTres = new Banco("Jarret Lafuente", 9582019689, "Conta Poupança", 27363);
+// let objetoLiteral = { titular: "Bendite Huggett", nConta: 2138105881, tipoConta: "Conta Poupança", saldo: 33196 };
+// let dadosConvertidos = JSON.stringify(objetoLiteral);
 
-let clienteQuatro = new Banco("Ansel Ardley", 1761924656, "Conta Poupança", 32415);
+// console.log(dadosConvertidos);
 
-let clienteCinco = new Banco("Jacki Shurmer", 7401971607, "Conta Poupança", 18789);
-
-let clienteSeis = new Banco("Jobi Mawtus", 630841470, "Conta Corrente", 28776);
-
-let clienteSete = new Banco("Thomasin Latour", 4223508636, "Conta Corrente", 2177);
-
-let clienteOito = new Banco("Lonnie Verheijden", 185979521, "Conta Poupança", 25994);
-
-let clienteNove = new Banco("Alonso Wannan", 3151956165, "Conta Corrente", 7601);
-
-let clienteDez = new Banco("Bendite Huggett", 2138105881, "Conta Poupança", 33196);
-
-let objetoLiteral = { titular: "Bendite Huggett", nConta: 2138105881, tipoConta: "Conta Poupança", saldo: 33196 };
-let dadosConvertidos = JSON.stringify(objetoLiteral);
-
-console.log(dadosConvertidos);
-
-dadosConvertidos = JSON.parse(dadosConvertidos);
-console.log(dadosConvertidos)
+// dadosConvertidos = JSON.parse(dadosConvertidos);
+// console.log(dadosConvertidos)
