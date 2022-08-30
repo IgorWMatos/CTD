dd4aff095b7a8e0ca8ebb777f6fd64b84f839688
Igor Matos <105871904+IgorWMatos@users.noreply.github.com>
2022-08-29 22:36:04 -0300

    alteracoes

--- 2_bimestre/programacao_imperativa/exAula13.js
+++ 2_bimestre/programacao_imperativa/exAula13.js
@@ -1,10 +1,3 @@
-function Banco(titular, nConta, tipoConta, saldo) {
-    this.titular = titular;
-    this.nConta = nConta;
-    this.tipoConta = tipoConta;
-    this.saldo = saldo;
-}
-
 let client1 = ['Abigael Natte', 5486273622, 'Conta Corrente', 27771]
 let client2 = ['Ramon Connell', 1183971869, 'Conta Poupança', 8675]
 let client3 = ['Jarret Lafuente', 9582019689, 'Conta Poupança', 27363]
@@ -16,21 +9,39 @@ let client8 = ['Lonnie Verheijden', 185979521, 'Conta Poupança', 25994]
 let client9 = ['Alonso Wannan', 3151956165, 'Conta Corrente', 7601]
 let client10 = ['Bendite Huggett', 2138105881, 'Conta Poupança', 33196]
 
-// let clienteUm = new Banco("Abigael Natte", 5486273622, "Conta Corrente", 27771);
-// let clienteDois = new Banco("Ramon Connell", 1183971869, "Poupança", 8675);
-// let clienteTres = new Banco("Jarret Lafuente", 9582019689, "Conta Poupança", 27363);
-// let clienteQuatro = new Banco("Ansel Ardley", 1761924656, "Conta Poupança", 32415);
-// let clienteCinco = new Banco("Jacki Shurmer", 7401971607, "Conta Poupança", 18789);
-// let clienteSeis = new Banco("Jobi Mawtus", 630841470, "Conta Corrente", 28776);
-// let clienteSete = new Banco("Thomasin Latour", 4223508636, "Conta Corrente", 2177);
-// let clienteOito = new Banco("Lonnie Verheijden", 185979521, "Conta Poupança", 25994);
-// let clienteNove = new Banco("Alonso Wannan", 3151956165, "Conta Corrente", 7601);
-// let clienteDez = new Banco("Bendite Huggett", 2138105881, "Conta Poupança", 33196);
 
-// let objetoLiteral = { titular: "Bendite Huggett", nConta: 2138105881, tipoConta: "Conta Poupança", saldo: 33196 };
-// let dadosConvertidos = JSON.stringify(objetoLiteral);
+/* -------------- */
 
-// console.log(dadosConvertidos);
+const conta = {
+    titular: 'Jose',
+    numeroConta: 234546,
+    tipo: 'corrente',
+    saldo: 1500.00,
+}
 
-// dadosConvertidos = JSON.parse(dadosConvertidos);
-// console.log(dadosConvertidos)
+const dados = [
+    {
+        "titular": "Abigael Natte",
+        "numero": 5486273622,
+        "tipo": "Conta Corrente",
+        "saldo": 27771
+    },
+
+    {
+        "titular": "Ramon Connell",
+        "numero": 1183971869,
+        "tipo": "Conta Poupança",
+        "saldo": 8675
+    },
+
+]
+
+function Banco(titular, nConta, tipoConta, saldo) {
+    this.titular = titular;
+    this.nConta = nConta;
+    this.tipoConta = tipoConta;
+    this.saldo = saldo;
+}
+
+let conta1 = new Banco('fulano', 124623, 'corrrente', 1500.95)
+console.log(conta1);
