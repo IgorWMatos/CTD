525fbb6bc56a8c0c83ba0cf4219edd4f392909d1
Igor <iw-matos@hotmail.com>
2022-09-14 12:23:13 -0300

    alterações

--- 2_bimestre/programacao_imperativa/ex17primeiroApp.js
+++ 2_bimestre/programacao_imperativa/ex17primeiroApp.js
@@ -14,6 +14,50 @@ let pessoas = [
     {
         altura: 1.52,
         sexo: 'F'
+    },
+    {
+        altura: 1.75,
+        sexo: 'F'
+    },
+    {
+        altura: 1.90,
+        sexo: 'M'
+    },
+    {
+        altura: 1.56,
+        sexo: 'F'
+    },
+    {
+        altura: 1.71,
+        sexo: 'F'
+    },
+    {
+        altura: 1.70,
+        sexo: 'M'
+    },
+    {
+        altura: 1.64,
+        sexo: 'F'
+    },
+    {
+        altura: 1.82,
+        sexo: 'M'
+    },
+    {
+        altura: 1.67,
+        sexo: 'F'
+    },
+    {
+        altura: 1.73,
+        sexo: 'M'
+    },
+    {
+        altura: 1.62,
+        sexo: 'M'
+    },
+    {
+        altura: 1.51,
+        sexo: 'F'
     }
 ]
 
