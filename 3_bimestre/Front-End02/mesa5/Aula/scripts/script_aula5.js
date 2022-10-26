f5c0ac3c232bddeae117dacd025cb2e7075db22c
Igor Matos <105871904+IgorWMatos@users.noreply.github.com>
2022-10-25 21:59:54 -0300

    alteracao

--- 3_bimestre/Front-End02/mesa5/Aula/scripts/script_aula5.js
+++ 3_bimestre/Front-End02/mesa5/Aula/scripts/script_aula5.js
@@ -1,10 +1,31 @@
 //@@@@@@ [1] - Modelos de texto  - Template Strings
 
+let usuario = "Igor";
+
+function boasvindas () {
+    return "Boas-vindas Igor";
+}
+
+// alert(`${boasvindas()} não esqueça de avaliar`); // `` aspas cruzadas permitem interpolação, permite que o javascript leia uma variável dentro de uma string sem concatená-la
+
+
+
 
 //@@@@@@ [2] - Modificando elementos com o DOM
 
+let h1 = document.querySelector("h1");
+h1.innerHTML = "<h4> Aula 05 </h4>"
+
 
 //@@@@@@ [3] - Modificando estilos 
+
+h1.style.color = "#000";
+h1.style.textAlign = "center"
+
+h1.classList.add("novoh1");
+
+let valid = h1.classList.contains("novoh1");
+console.log(valid);
 
 
 //@@@@@@ [4] Modificando a classe dos elementos
